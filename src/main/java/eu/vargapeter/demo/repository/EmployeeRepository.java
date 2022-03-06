38694f53fa83b6cd16a145ce511f92904c765b39
pepe <vpeter3@gmail.com>
2022-03-06 13:07:27 +0100

    add Employee repository

---
+++ src/main/java/eu/vargapeter/demo/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package eu.vargapeter.demo.repository;
+
+import eu.vargapeter.demo.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    Optional<Employee> findEmployeeByEmail(String email);
+}
