8aa481e523eaecc3c6afc554aaf546048d1bebb9
pepe <vpeter3@gmail.com>
2022-03-06 21:58:42 +0100

    add Constans

---
+++ src/main/java/eu/vargapeter/demo/util/Constans.java
@@ -0,0 +1,12 @@
+package eu.vargapeter.demo.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class Constans {
+
+    public final static List<Locale> LOCALES = Arrays.asList(
+            new Locale("en"),
+            new Locale("hu"));
+}
