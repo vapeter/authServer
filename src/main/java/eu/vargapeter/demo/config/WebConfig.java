ea06684d499008775d0a1c6e6da96f3f56120ada
pepe <vpeter3@gmail.com>
2022-03-06 21:59:09 +0100

    add WebConfig

---
+++ src/main/java/eu/vargapeter/demo/config/WebConfig.java
@@ -0,0 +1,38 @@
+package eu.vargapeter.demo.config;
+
+
+import eu.vargapeter.demo.util.Constans;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends AcceptHeaderLocaleResolver implements WebMvcConfigurer {
+
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+        String headerLang = request.getHeader("Accept-Language");
+        return headerLang == null || headerLang.isEmpty()
+                ? Locale.getDefault()
+                : Locale.lookup(Locale.LanguageRange.parse(headerLang), Constans.LOCALES);
+    }
+
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
+
+        return messageSource;
+    }
+}
