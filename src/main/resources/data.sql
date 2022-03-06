a8e4d1fdca39d8616b25d5cb96b3ee8677d7b22d
pepe <vpeter3@gmail.com>
2022-03-06 18:15:23 +0100

    add default admin in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,7 @@
 INSERT INTO roles (role_id, name) VALUES (0, 'ADMIN');
 INSERT INTO roles (role_id, name) VALUES (1, 'ROLE_USER');
+
+INSERT INTO employees (employee_id, name, email, mobil, password)
+    VALUES (0, 'Admin', 'admin@admin.hu', '+3600123456789', '$2a$10$onOxWqaZn.951Sjq9NtY8eIq6.jx/4QERRhncmfkJCz/xavDAmxxS');
+
+INSERT INTO employees_has_roles (employee_id, role_id) VALUES (0, 0);
