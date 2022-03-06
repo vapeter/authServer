57efc02b04770cbb05e6241b7d5e6cd71589045b
pepe <vpeter3@gmail.com>
2022-03-06 18:05:43 +0100

    add Bad Credentials message

--- src/main/java/eu/vargapeter/demo/controller/LoginController.java
+++ src/main/java/eu/vargapeter/demo/controller/LoginController.java
@@ -1,11 +1,15 @@
 package eu.vargapeter.demo.controller;
 
+import eu.vargapeter.demo.config.Translator;
 import eu.vargapeter.demo.model.dto.LoginRequest;
 import eu.vargapeter.demo.security.AuthResponse;
 import eu.vargapeter.demo.service.LoginService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Slf4j
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class LoginController {
@@ -27,7 +32,16 @@ public class LoginController {
     @PostMapping("/login")
     public ResponseEntity authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
-        String token = loginService.loginUser(loginRequest);
+        String token;
+
+        try {
+            token = loginService.loginUser(loginRequest);
+        } catch (BadCredentialsException e) {
+            return new ResponseEntity<>(
+                    Translator.toLocale("log.loginError"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
         return ResponseEntity.ok(new AuthResponse(token));
     }
 }
